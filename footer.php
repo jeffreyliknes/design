411cdcdbcc0d891780dba2dbdff88c2bf8990d92
Jeff Liknes <jeff.liknes@gmail.com>
2019-04-13 16:43:38 -0600

    init

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="site-info">
+					<div>
+					<i class="fa fa-envelope-open-o"></i>			
+					</div>
+				</div><!-- .site-info -->
+
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
